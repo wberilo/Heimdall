53fbe1a756a7e61c9a9fb1307e20b4268c69c195
wberilo <wberilo@gmail.com>
2021-09-02 19:20:39 -0300

    but who

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ discordClient.on('messageReactionAdd', async (reaction, user) => {
 	// Now the message has been cached and is fully available
 	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction! from`);
   console.log(reaction);
+  console.log(reaction.users);
 	// The reaction is now also fully available and the properties will be reflected accurately:
 	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
 });
