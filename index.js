157411ba8d00be4e3079f1aea6f2aa1d80a387f5
wberilo <wberilo@gmail.com>
2021-09-02 20:06:22 -0300

    fr

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ discordClient.on('messageReactionAdd', async (reaction, user) => {
     console.log(reaction);
     const role = await reaction.message.guild.roles.cache.find(role => role.name === "Adventurer")
     //console.log(role);
-    a.forEach(user => reaction.message.guild.member(user).options.getMember.roles.add(role))
+    a.forEach(user => reaction.message.guild.member(user).roles.add(role))
     //const role = reaction.message.author.guild.roles.cach
     //a.forEach(user => )
     
