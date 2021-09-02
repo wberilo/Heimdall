3bbd2b7c894ab1e9d63f446c7d8b8941ed1845a1
wberilo <wberilo@gmail.com>
2021-09-02 19:16:30 -0300

    push

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ discordClient.on('messageReactionAdd', async (reaction, user) => {
 	}
 
 	// Now the message has been cached and is fully available
-	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction! from ${reaction.user.username}`);
+	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction! from ${reaction.user}`);
 	// The reaction is now also fully available and the properties will be reflected accurately:
 	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
 });
